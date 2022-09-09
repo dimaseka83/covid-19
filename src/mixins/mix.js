bff85fdae939f2bc5dd4b4e9909dfad76ee91236
dhimaseka <dimaseka83@gmail.com>
2022-09-09 12:40:34 +0700

    ++

--- src/mixins/mix.js
+++ src/mixins/mix.js
@@ -4,4 +4,9 @@ export default {
             api : 'https://covid19.mathdro.id/api'
         }
     },
+    methods: {
+        splitNumber(number){
+            return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+          },
+    },
 }
